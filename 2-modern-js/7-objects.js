d3b39162884d54f9275d4b5b97dfb3ec0c461741
Roberts Sukonovs <roberts.sukonovs@tet.lv>
2020-10-02 18:27:00 +0300

    Tinkering around.

--- 2-modern-js/7-objects.js
+++ 2-modern-js/7-objects.js
@@ -1,6 +1,14 @@
+const PI = Math.PI;
+const someVar = 'test';
+
 const obj = {
   p1: 10,
   p2: 20,
   f1() {},
   f2: () => {},
+  PI,
+  [someVar]: 1000,
 };
+
+console.log(obj.PI);
+console.log(obj.test);
